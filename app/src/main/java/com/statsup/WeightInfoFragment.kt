8a5ca072d7ec7ea89e8a5150fe46bbd4c5a9784c
Marco Fracassi <fracassi.marco@gmail.com>
2020-01-23 19:03:11 +0100

    Add weight info

---
+++ app/src/main/java/com/statsup/WeightInfoFragment.kt
@@ -0,0 +1,78 @@
+package com.statsup
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.overview_item.view.*
+import kotlinx.android.synthetic.main.weight_info_fragment.view.*
+import java.util.*
+
+
+class WeightInfoFragment : Fragment() {
+
+    private lateinit var weights: List<Weight>
+    private lateinit var weightOverviewItem: View
+    private lateinit var howLongOverviewItem: View
+
+    private val weightListener = object : Listener<List<Weight>> {
+        override fun update(subject: List<Weight>) {
+            weights = subject.sortedBy { it.dateInMillis }
+
+            updateUi(weights)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUi(weights: List<Weight>) {
+        weightOverviewItem.left_text.text = "Numero di pesate"
+        weightOverviewItem.left_value.text = weights.size.toString()
+
+        howLongOverviewItem.left_text.text = "Prima pesata"
+        howLongOverviewItem.left_value.text = weights.first().date().toString("dd/MM/yyyy")
+        howLongOverviewItem.left_value.textSize = 21f
+        howLongOverviewItem.left_value.setTextColor(Color.BLACK)
+
+        howLongOverviewItem.right_text.text = "Ultima pesata"
+        howLongOverviewItem.right_value.text = weights.last().date().toString("dd/MM/yyyy")
+        howLongOverviewItem.right_value.textSize = 21f
+        howLongOverviewItem.right_value.setTextColor(Color.BLACK)
+
+        howLongOverviewItem.center_text.text = "Da"
+        howLongOverviewItem.center_value.textSize = 16f
+        howLongOverviewItem.center_value.text = since(weights)
+    }
+
+    private fun since(weights: List<Weight>): String {
+        if(weights.isEmpty())
+            return "0 anni 0 mesi"
+
+        val firstMeasure = GregorianCalendar().apply { time = weights.first().date().toDate() }
+        val today = GregorianCalendar().apply { time = Date() }
+        val yearsInBetween = today.get(Calendar.YEAR) - firstMeasure.get(Calendar.YEAR)
+        val monthsDiff = today.get(Calendar.MONTH) - firstMeasure.get(Calendar.MONTH)
+        val ageInMonths = yearsInBetween * 12 + monthsDiff
+        val years = ageInMonths / 12
+        val months = ageInMonths - (12 * years)
+        return "$years anni $months mesi"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.weight_info_fragment, container, false)
+        weightOverviewItem  = view.weightOverviewItem
+        howLongOverviewItem = view.howLongOverviewItem
+
+        WeightRepository.listen(weightListener)
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        WeightRepository.removeListener(weightListener)
+    }
+}
--- app/src/main/java/com/statsup/WeightTabs.kt
+++ app/src/main/java/com/statsup/WeightTabs.kt
@@ -5,7 +5,8 @@ import android.support.v4.app.Fragment
 
 enum class WeightTabs(val position: Int, val label: String, val color: Int, val fragment: Fragment) {
     BALANCE(0, "Peso", Color.rgb(76, 175, 80), BalanceFragment()),
-    BMI(1, "BMI", Color.rgb(33, 150, 243), BmiFragment());
+    BMI(1, "BMI", Color.rgb(33, 150, 243), BmiFragment()),
+    INFO(2, "Info", Color.rgb(244, 67, 54), WeightInfoFragment());
 
     companion object {
         fun at(position: Int): WeightTabs {
