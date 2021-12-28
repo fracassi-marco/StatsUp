11f24fee6c484abc2b4372a613c84db56dde81e2
Marco Fracassi <fracassi.marco@gmail.com>
2021-12-28 12:07:01 +0100

    Add interactive map in activity details

--- app/src/main/java/com/statsup/ActivityDetailsActivity.kt
+++ app/src/main/java/com/statsup/ActivityDetailsActivity.kt
@@ -1,39 +1,39 @@
 package com.statsup
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.view.View.VISIBLE
 import android.widget.GridLayout
+import android.widget.GridLayout.GONE
 import android.widget.GridLayout.UNDEFINED
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.activity_details_item.view.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import org.joda.time.format.DateTimeFormat
 
-class ActivityDetailsActivity : AppCompatActivity() {
 
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.Main + job)
+class ActivityDetailsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var activity: Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        val activity = ActivityRepository.byId(intent.getLongExtra("id", -1))
+        activity = ActivityRepository.byId(intent.getLongExtra("id", -1))
 
-        scope.launch {
-            val map = MapRepository.ofActivity(applicationContext, activity)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-            if (map != null) {
-                activity_map.visibility = VISIBLE
-                activity_map.setImageBitmap(map)
-            }
-        }
         activity_title.text = activity.title
         sport_icon.setImageResource(activity.sport.icon)
         sport_name.setText(activity.sport.title)
@@ -121,8 +121,28 @@ class ActivityDetailsActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        job.cancel()
+    override fun onMapReady(googleMap: GoogleMap) {
+        if (activity.map == null) {
+            val f: SupportMapFragment =
+                supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            f.view!!.visibility = GONE
+            return
+        }
+
+        val list = PolyUtil.decode(activity.map!!)
+        googleMap.addPolyline(
+            PolylineOptions()
+                .width(5.toFloat())
+                .color(Color.BLUE)
+                .geodesic(true)
+                .addAll(list)
+        )
+
+        val builder: LatLngBounds.Builder = LatLngBounds.Builder()
+        list.forEach {
+            builder.include(it)
+        }
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 50))
     }
 }
