7d2ec5b4a2188200a0924b081495f4f990bfce8d
Marco Fracassi <fracassi.marco@gmail.com>
2022-04-01 23:31:11 +0200

    add circles

--- app/src/main/java/com/statsup/ActivityDashboardFragment.kt
+++ app/src/main/java/com/statsup/ActivityDashboardFragment.kt
@@ -1,11 +1,18 @@
 package com.statsup
 
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hookedonplay.decoviewlib.charts.SeriesItem
+import com.hookedonplay.decoviewlib.events.DecoEvent
+import com.statsup.Content.showActivitiesOrEmptyPage
+import com.statsup.Variation.percentage
 import com.statsup.databinding.ActivityDashboardFragmentBinding
+
 
 class ActivityDashboardFragment : Fragment() {
 
@@ -20,13 +27,61 @@ class ActivityDashboardFragment : Fragment() {
         _binding = ActivityDashboardFragmentBinding.inflate(inflater, container, false)
         setHasOptionsMenu(false)
 
-        val activities = ActivityRepository.ofMonth(Month().previous())
-        binding.value1.text = Measure.frequency(Stats.FREQUENCY.provider(activities))
-        binding.value2.text = Measure.hm(activities.sumOf { activity -> activity.durationInSeconds })
-        binding.value3.text = Measure.of(Stats.DISTANCE.provider(activities), "Km", "", "- ")
+        val currentActivities = ActivityRepository.ofMonth(Month().previous().previous().previous().previous())
+        binding.frequencyValue.text = Measure.frequency(Stats.FREQUENCY.provider(currentActivities))
+        binding.durationValue.text = Measure.hm(currentActivities.sumOf { activity -> activity.durationInSeconds })
+        binding.distanceValue.text = Measure.of(Stats.DISTANCE.provider(currentActivities), "Km", "", "- ")
 
-        Content.showActivitiesOrEmptyPage(binding.noActivitiesLayout, binding.content)
+        val previousActivities = ActivityRepository.ofMonth(Month().previous().previous().previous().previous().previous())
+        val frequency = percentage(Stats.FREQUENCY.provider(currentActivities), Stats.FREQUENCY.provider(previousActivities)).toDouble()
+        val duration = percentage(currentActivities.sumOf { a -> a.durationInSeconds }, previousActivities.sumOf { a -> a.durationInSeconds })
+        val distance = percentage(Stats.DISTANCE.provider(currentActivities), Stats.DISTANCE.provider(previousActivities))
+        val s = ((frequency * 2) + duration + (distance * 0.5)) / 3.5
+        val stamina = addCircle(0f, Color.parseColor("#fc5200"), s.toFloat(), 64f)
+        addCircle(74f, Stats.FREQUENCY.color, frequency.toFloat(), 32f)
+        addCircle(116f, Stats.DURATION.color, duration.toFloat(), 32f)
+        addCircle(158f, Stats.DISTANCE.color, distance.toFloat(), 32f)
+        addLabel(stamina)
+
+        showActivitiesOrEmptyPage(binding.noActivitiesLayout, binding.content)
 
         return binding.root
+    }
+
+    private fun addLabel(stamina: SeriesItem) {
+        stamina.addArcSeriesItemListener(object : SeriesItem.SeriesItemListener {
+            override fun onSeriesItemAnimationProgress(
+                percentComplete: Float,
+                currentPosition: Float
+            ) {
+                val percentFilled = (currentPosition - stamina.minValue) / (stamina.maxValue - stamina.minValue)
+                binding.percent.text = String.format("%.0f%%", percentFilled * 100f)
+            }
+
+            override fun onSeriesItemDisplayProgress(percentComplete: Float) {
+            }
+        })
+    }
+
+    private fun addCircle(inset: Float, color: Int, value: Float, width: Float): SeriesItem {
+        binding.arc.addSeries(
+            SeriesItem.Builder(Color.argb(255, 218, 218, 218))
+                .setRange(0f, 100f, 100f)
+                .setInset(PointF(inset, inset))
+                .setLineWidth(width)
+                .build()
+        )
+
+        val series = SeriesItem.Builder(color)
+            .setRange(0f, 100f, 0f)
+            .setInset(PointF(inset, inset))
+            .setLineWidth(width)
+            .build()
+
+        val seriesIndex = binding.arc.addSeries(series)
+
+        binding.arc.addEvent(DecoEvent.Builder(value).setIndex(seriesIndex).setDelay((inset.toLong() * 10) + 50).build())
+
+        return series
     }
 }
--- app/src/main/java/com/statsup/Variation.kt
+++ app/src/main/java/com/statsup/Variation.kt
@@ -6,6 +6,16 @@ object Variation {
     fun percentage(actualCounter: Double, allCounter: Double): Int {
         if(allCounter == 0.0)
             return 0
+        if(actualCounter > allCounter)
+            return 100
+        return (actualCounter / allCounter.toFloat() * 100).roundToInt()
+    }
+
+    fun percentage(actualCounter: Int, allCounter: Int): Int {
+        if(allCounter == 0)
+            return 0
+        if(actualCounter > allCounter)
+            return 100
         return (actualCounter / allCounter.toFloat() * 100).roundToInt()
     }
 }
