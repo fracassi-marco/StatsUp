f84ae2680b020bee05f657011c65ee3419fd0666
Marco Fracassi <fracassi.marco@gmail.com>
2022-04-04 10:51:11 +0200

    1.9.2 Add labels in dashboard

--- app/src/main/java/com/statsup/ActivityDashboardFragment.kt
+++ app/src/main/java/com/statsup/ActivityDashboardFragment.kt
@@ -12,12 +12,14 @@ import com.hookedonplay.decoviewlib.events.DecoEvent
 import com.statsup.Content.showActivitiesOrEmptyPage
 import com.statsup.Variation.percentage
 import com.statsup.databinding.ActivityDashboardFragmentBinding
+import java.util.*
 
 
 class ActivityDashboardFragment : Fragment() {
 
     private var _binding: ActivityDashboardFragmentBinding? = null
     private val binding get() = _binding!!
+    private val labels: Queue<String> = LinkedList(listOf("frequency", "duration", "distance", "stamina"))
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,12 +38,28 @@ class ActivityDashboardFragment : Fragment() {
         val frequency = percentage(Stats.FREQUENCY.provider(currentActivities), Stats.FREQUENCY.provider(previousActivities)).toDouble()
         val duration = percentage(currentActivities.sumOf { a -> a.durationInSeconds }, previousActivities.sumOf { a -> a.durationInSeconds })
         val distance = percentage(Stats.DISTANCE.provider(currentActivities), Stats.DISTANCE.provider(previousActivities))
-        val s = ((frequency * 2) + duration + (distance * 0.5)) / 3.5
-        val stamina = addCircle(0f, Color.parseColor("#fc5200"), s.toFloat(), 64f)
+        val staminaValue = ((frequency * 2) + duration + (distance * 0.5)) / 3.5
+        val stamina = addCircle(0f, requireContext().getColor(R.color.stamina), staminaValue.toFloat(), 64f)
         addCircle(74f, Stats.FREQUENCY.color, frequency.toFloat(), 32f)
         addCircle(116f, Stats.DURATION.color, duration.toFloat(), 32f)
         addCircle(158f, Stats.DISTANCE.color, distance.toFloat(), 32f)
         addLabel(stamina)
+
+        binding.arc.setOnClickListener {
+            val next = labels.remove()
+            labels.add(next)
+            val value = when (next) {
+                "frequency" -> frequency
+                "duration" -> duration.toDouble()
+                "distance" -> distance.toDouble()
+                else -> staminaValue
+            }
+
+            binding.percent.setTextColor(requireContext().getColor(resources.getIdentifier(next, "color", requireContext().packageName)))
+            binding.percent.text = String.format("%.0f%%", value)
+            binding.percentLabel.text = getString(resources.getIdentifier(next, "string", requireContext().packageName))
+            binding.percentLabel.setTextColor(requireContext().getColor(resources.getIdentifier(next, "color", requireContext().packageName)))
+        }
 
         showActivitiesOrEmptyPage(binding.noActivitiesLayout, binding.content)
 
@@ -56,6 +74,9 @@ class ActivityDashboardFragment : Fragment() {
             ) {
                 val percentFilled = (currentPosition - stamina.minValue) / (stamina.maxValue - stamina.minValue)
                 binding.percent.text = String.format("%.0f%%", percentFilled * 100f)
+                binding.percent.setTextColor(requireContext().getColor(R.color.stamina))
+                binding.percentLabel.text = getString(R.string.stamina)
+                binding.percentLabel.setTextColor(requireContext().getColor(R.color.stamina))
             }
 
             override fun onSeriesItemDisplayProgress(percentComplete: Float) {
