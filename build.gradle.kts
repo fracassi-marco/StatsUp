44397818698d8d905453f3803737642c5e27c0ef
Marco Fracassi <fracassi.marco@gmail.com>
2026-03-25 09:56:40 +0100

    Add Ben Manes Versions plugin and update Gradle wrapper to 9.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.ben.manes.versions)
     alias(libs.plugins.compose.compiler) apply false
     id("com.google.devtools.ksp") version "2.3.6" apply false
 }
