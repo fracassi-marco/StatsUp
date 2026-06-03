aad321c55cd100955fae104e504dc304c94cfe3a
Marco Fracassi <fracassi.marco@gmail.com>
2026-06-03 17:17:14 +0200

    Release v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.statsup"
         minSdk = 34
         targetSdk = 37
-        versionCode = 10812
-        versionName = "1.8.12"
+        versionCode = 10900
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
