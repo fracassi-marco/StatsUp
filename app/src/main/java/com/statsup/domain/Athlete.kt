78c945bd8e4e030c246ef539eedd093f2d91d626
Marco Fracassi <fracassi.marco@gmail.com>
2026-06-03 10:54:23 +0200

    ✨Make username nullable in Athlete model and update ProfileScreen to handle null values

--- app/src/main/java/com/statsup/domain/Athlete.kt
+++ app/src/main/java/com/statsup/domain/Athlete.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class Athlete(
     @PrimaryKey var id: Long,
-    var username: String,
+    var username: String? = null,
     var resourceState: Int? = null,
     var profileMedium: String? = null,
     var profile: String? = null
