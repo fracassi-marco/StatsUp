eeb69b96828d59bebc7a6cf85f8f5a5cf3079fa4
Marco Fracassi <fracassi.marco@gmail.com>
2026-08-03 12:33:19 +0200

    Release v2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.statsup"
         minSdk = 34
         targetSdk = 37
-        versionCode = 20502
-        versionName = "2.5.2"
+        versionCode = 20600
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
